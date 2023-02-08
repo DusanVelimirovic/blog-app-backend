0c7e7c69796354672066b3bfe7d3508498c92c2b
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-02-08 19:23:25 +0100

    Create auth route [/register, /login]

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const app = express();
 const port = 5000;
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+
+//allow us to send any json object
+app.use(express.json());
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("Connected to Mongoose"))
 .catch(err => console.log(err));
+
+//auth route
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
